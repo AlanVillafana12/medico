5f7c01b0424589c2bda19cb86682f62b6ba21d7b
alanv <am.lopezvillafana@ugto.mx>
2024-04-09 19:28:29 -0600

    Citas Pacientes

--- Medico/Data/ApplicationDbContext.cs
+++ Medico/Data/ApplicationDbContext.cs
@@ -1,3 +1,5 @@
+using Medico.Models;
+using Medico.NewFolder;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,5 +7,13 @@ namespace Medico.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Cita> Citas { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+
     }
 }
---
+++ Medico/Models/Paciente.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Medico.NewFolder
+{
+    public class Paciente
+    {
+        public int  Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del paciente es requerido")]
+        public string Nombre { get; set; }
+        
+        [Required(ErrorMessage = "El Primer Apellido del paciente es requerido")]
+        public string PrimerApellido { get; set; }
+
+        public string? SegundoApellido { get; set; }
+
+        [Required(ErrorMessage = "La {0} del paciente es requerido")]
+        [MinLength(18,ErrorMessage = "La {0} debe de tener {1} caracteres")]
+        [MaxLength(18,ErrorMessage = "La {0} debe de tener {1} caracteres")]
+        public string CURP { get; set; }
+
+        [Required(ErrorMessage = "El Tipo de sangre del paciente es requerido")]
+        public string TipoSangre { get; set; }
+
+
+        public string? Observaciones { get; set; }
+
+    }
+}
